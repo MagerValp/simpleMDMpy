39f9c4c5af96c80ca9d2566cd8cf1dbb3051fe41
Per Olofsson <magervalp@fastmail.fm>
2022-05-20 09:47:50 +0200

    Add support for `include_awaiting_enrollment` in `get_device()`

--- SimpleMDMpy/Devices.py
+++ SimpleMDMpy/Devices.py
@@ -11,13 +11,15 @@ class Devices(SimpleMDMpy.SimpleMDM.Connection):
         SimpleMDMpy.SimpleMDM.Connection.__init__(self, api_key)
         self.url = self._url("/devices")
 
-    def get_device(self, device_id="all", search=None):
+    def get_device(self, device_id="all", search=None, include_awaiting_enrollment=False):
         """Returns a device specified by id. If no ID or search is
         specified all devices will be returned"""
         url = self.url
-        data = None
+        data = {}
         if search:
-            data = {'search': search}
+            data['search'] = search
+        if include_awaiting_enrollment:
+            data['include_awaiting_enrollment'] = True
         elif device_id != 'all':
             url = url + "/" + str(device_id)
         return self._get_data(url, data)
