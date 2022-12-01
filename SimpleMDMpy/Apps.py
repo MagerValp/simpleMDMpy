1fcb06a0dc340402b383b27a2666ea6f22528624
Per Olofsson <magervalp@fastmail.fm>
2022-12-01 14:12:50 +0100

    Rename legacy actions in Apps

--- SimpleMDMpy/Apps.py
+++ SimpleMDMpy/Apps.py
@@ -4,6 +4,7 @@
 #pylint: disable=invalid-name
 
 import SimpleMDMpy.SimpleMDM
+from SimpleMDMpy.utils import legacy
 
 class Apps(SimpleMDMpy.SimpleMDM.Resource):
     """ apps module for SimpleMDMpy"""
@@ -11,15 +12,28 @@ class Apps(SimpleMDMpy.SimpleMDM.Resource):
         super().__init__(api_key)
         self.url = self.api_url("/apps")
 
+    @legacy("get")
     def get_app(self, app_id="all"):
         """list app, if none specified all return"""
+        return self.get(app_id)
+
+    def get(self, app_id="all"):
+        """Retrieve the specified app, or a list of all if no app_id is
+        specified"""
         url = self.url
         if app_id != 'all':
             url = url + "/" + app_id
         return self.get_data(url)
 
+    @legacy("create")
     def create_app(self, name=None, app_store_id=None, bundle_id=None, binary=None):
         """upload an app binary"""
+        return self.create(name, app_store_id, bundle_id, binary)
+
+    def create(self, name=None, app_store_id=None, bundle_id=None, binary=None):
+        """You can use this method to add an App Store app, upload an enterprise iOS app, or upload macOS package to your app catalog.
+        One and only one of of app_store_id, bundle_id, or binary must be specified. Name can optionally be specified if binary
+        is specified."""
         data = {}
         files = {}
         if name:
@@ -32,8 +46,13 @@ class Apps(SimpleMDMpy.SimpleMDM.Resource):
             files['binary'] = open(binary, 'rb')
         return self.post_data(self.url, data, files)
 
+    @legacy("update")
     def update_app(self, app_id, binary=None, name=None):
         """update an apps info binary etc"""
+        return self.update(app_id, binary, name)
+
+    def update(self, app_id, binary=None, name=None):
+        """You can use this method to update the binary of an existing app."""
         url = self.url + "/" + app_id
         data = {}
         files = {}
@@ -43,8 +62,12 @@ class Apps(SimpleMDMpy.SimpleMDM.Resource):
             files['binary'] = open(binary, 'rb')
         return self.patch_data(url, data, files)
 
+    @legacy("delete")
     def delete_app(self, app_id):
         """delete an app"""
+        return self.delete(app_id)
+
+    def delete(self, app_id):
         url = self.url + "/" + app_id
         data = {}
         return self.delete_data(url, data) #pylint: disable=too-many-function-args
