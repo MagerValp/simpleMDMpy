370f710ecda3fcd2b81a22c9870f7a39b8d48cb6
Per Olofsson <magervalp@fastmail.fm>
2025-10-29 14:19:42 +0100

    Make include_awaiting_enrollment stringly typed

--- SimpleMDMpy/Devices.py
+++ SimpleMDMpy/Devices.py
@@ -30,7 +30,10 @@ class Devices(SimpleMDMpy.SimpleMDM.Connection):
             array: An array of dictionary objects with device information.
         """
         url = self.url
-        params = {'include_awaiting_enrollment': include_awaiting_enrollment}
+        if include_awaiting_enrollment:
+            params = {"include_awaiting_enrollment": "true"}
+        else:
+            params = {}
         # if a device ID is specified, then ignore any searches
         if device_id != 'all':
             url = url + "/" + str(device_id)
